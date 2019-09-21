f34e8682b8cca0701b5e474a0c51aaf1c67978ef
louchenyao <louchenyao@gmail.com>
2019-09-21 20:54:01 +0800

    add the hash family mircobenchmark

--- BUILD
+++ BUILD
@@ -1,10 +1,11 @@
 cc_binary(
     name = "bench",
     srcs = glob(["benchmarks/*.cpp"]),
-    copts = ["-std=c++17", "-O3", "-march=native", "-Wall", "-Wextra", "-Werror"],
+    copts = ["-std=c++17", "-O2", "-march=native", "-Wall", "-Wextra", "-Werror"],
     deps = [
         "@benchmark//:benchmark", 
         "@benchmark//:benchmark_main",
+        "//:ssfehash",
     ]
 )
 
--- benchmarks/bench.cpp
+++ benchmarks/bench.cpp
@@ -6,6 +6,8 @@
 #include <map>
 #include <vector>
 
+#include "ssfehash/hash_family.h"
+
 static void BM_stdmap(benchmark::State& state) {
     // Setup
     std::vector<uint64_t> keys;
@@ -30,7 +32,37 @@ static void BM_stdmap(benchmark::State& state) {
     }
 }
 
-// Register the function as a benchmark
+static void BM_hash_family(benchmark::State& state) {
+    std::vector<int> families;
+    for (int i = 0; i < HASH_FAMILY_NUM; i++) {
+        families.push_back(i);
+    }
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(families.begin(), families.end(), g);
+
+    if (state.range(0) == 64) {
+        auto h = HashFamily<uint64_t>();
+        int i = 0;
+        for (auto _ : state) {
+            benchmark::DoNotOptimize(h.hash(42414242424u, families[i], 12345));
+            i = (i + 1) % HASH_FAMILY_NUM;
+        }
+    } else if (state.range(0) == 128) {
+        auto h = HashFamily<long double>();
+        assert(sizeof(long double) == 128/8);
+        int i = 0;
+        for (auto _ : state) {
+            benchmark::DoNotOptimize(h.hash(42414242424u, families[i], 12345));
+            i = (i + 1) % HASH_FAMILY_NUM;
+        }
+    } else {
+        assert(false);
+    }
+}
+
+BENCHMARK(BM_hash_family)->Arg(64)->Arg(128);
 BENCHMARK(BM_stdmap)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
+
 // Run the benchmark
 BENCHMARK_MAIN();
