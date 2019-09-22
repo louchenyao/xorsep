48e2981e602c14904a2562782029df00c13f099a
louchenyao <louchenyao@gmail.com>
2019-09-22 11:16:48 +0800

    more micro benchamrks

--- ssfehash/ssfe_bench.cpp
+++ ssfehash/ssfe_bench.cpp
@@ -35,6 +35,23 @@ static void BM_ssfe_query(benchmark::State& state) {
     }
 }
 
+static void BM_ssfe_build(benchmark::State& state) {
+    auto kvs = construct_keyvalues(state.range(0));
+    SSFE<uint64_t> ssfe(kvs.size());
+    shuffle_vector<std::pair<uint64_t, bool> >(kvs);
+    
+    // Benchmark
+    for (auto _ : state) {
+        ssfe.build(kvs);
+        // ensure the compiler won't optimize the build function
+        benchmark::DoNotOptimize(ssfe.query(kvs[0].first));
+    }
+
+    // HACK, it is keys per second
+    state.SetBytesProcessed(int64_t(state.iterations()) *
+                            int64_t(state.range(0)));
+}
+
 static void BM_query_prepare(benchmark::State& state) {
     auto kvs = construct_keyvalues(state.range(0));
     shuffle_vector<std::pair<uint64_t, bool> >(kvs);
@@ -47,6 +64,7 @@ static void BM_query_prepare(benchmark::State& state) {
     }
 }
 
+BENCHMARK(BM_ssfe_build)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
 BENCHMARK(BM_query_prepare)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
 BENCHMARK(BM_ssfe_query)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
 BENCHMARK(BM_stdmap_query)->Arg(10 * 1000)->Arg(100 * 1000)->Arg(1000 * 1000)->Arg(2 * 1000 * 1000);
