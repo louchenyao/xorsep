fe4721fcab8ea8897b655fa1f8fc61608a41e49a
louchenyao <louchenyao@gmail.com>
2019-09-29 00:04:17 +0800

    use the counter as the query key

--- ssfehash/ssfe_bench.cpp
+++ ssfehash/ssfe_bench.cpp
@@ -3,45 +3,52 @@
 #include "ssfehash/ssfe.h"
 #include "dev_utils/dev_utils.h"
 
+// BM_stdmap_query benchmarks the time of acquiring the equery key.
+// The result shows it consumes about 8 ns. I guess the overhead is from the extream long vector.
+// Thus, we set keys to [0, n), then use the conuter as the key for each iteration.
+
 static void BM_stdmap_query(benchmark::State& state) {
     // Setup
-    auto kvs = generate_keyvalues(state.range(0));
+    auto kvs = generate_continous_keyvalues(state.range(0));
     std::map<uint64_t, bool> m;
     for (const auto&kv : kvs) {
         m[kv.first] = kv.second;
     }
 
-    shuffle_vector<std::pair<uint64_t, bool> >(kvs);
-    
     // Benchmark
     int i = 0;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(m[kvs[i].first]);
-        i = (i + 1) % kvs.size();
+        // !!! stdmap may take this advantage since the adjacent keys may be stored in the adjacent memory
+        benchmark::DoNotOptimize(m[i]);
+        i += 1;
+        if (i == state.range(0)) {
+            i = 0;
+        }
     }
 }
 
 template <class SSFE_T>
 static void BM_ssfe_query(benchmark::State& state) {
-    auto kvs = generate_keyvalues(state.range(0));
+    auto kvs = generate_continous_keyvalues(state.range(0));
     SSFE_T ssfe(kvs.size());
     ssfe.build(kvs);
-    shuffle_vector<std::pair<uint64_t, bool> >(kvs);
     
     // Benchmark
     int i = 0;
     for (auto _ : state) {
-        benchmark::DoNotOptimize(ssfe.query(kvs[i].first));
-        i = (i + 1) % kvs.size();
+        benchmark::DoNotOptimize(ssfe.query(i));
+        i = i + 1;
+        if (i == state.range(0)) {
+            i = 0;
+        }
     }
 }
 
 template <class SSFE_T>
 static void BM_ssfe_query_batch(benchmark::State& state) {
-    auto kvs = generate_keyvalues(state.range(0));
+    auto kvs = generate_continous_keyvalues(state.range(0));
     SSFE_T ssfe(kvs.size());
     ssfe.build(kvs);
-    shuffle_vector<std::pair<uint64_t, bool> >(kvs);
     
     // Benchmark
     int i = 0;
@@ -49,8 +56,11 @@ static void BM_ssfe_query_batch(benchmark::State& state) {
         uint64_t keys[16];
         bool res[16];
         for (int j = 0; j < 16; ++j) {
-            keys[j] = kvs[i].first;
-            i = (i + 1) % kvs.size();
+            keys[j] = i;
+            i += 1;
+        }
+        if (i + 16 >= state.range(0)) {
+            i = 0;
         }
         ssfe.query_batch(keys, res, 16);
         benchmark::DoNotOptimize(res[0]);
@@ -59,7 +69,7 @@ static void BM_ssfe_query_batch(benchmark::State& state) {
 
 template <class SSFE_T>
 static void BM_ssfe_build(benchmark::State& state) {
-    auto kvs = generate_keyvalues(state.range(0));
+    auto kvs = generate_continous_keyvalues(state.range(0));
     SSFE_T ssfe(kvs.size());
     shuffle_vector<std::pair<uint64_t, bool> >(kvs);
     
@@ -76,7 +86,7 @@ static void BM_ssfe_build(benchmark::State& state) {
 }
 
 static void BM_query_prepare(benchmark::State& state) {
-    auto kvs = generate_keyvalues(state.range(0));
+    auto kvs = generate_continous_keyvalues(state.range(0));
     shuffle_vector<std::pair<uint64_t, bool> >(kvs);
     
     // Benchmark
