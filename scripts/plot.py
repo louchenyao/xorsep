f04961a048cb68c3c47a973af2f115d3a5cf1f38
louchenyao <louchenyao@gmail.com>
2020-01-15 22:18:01 +0800

    plot LLC misses

--- scripts/plot.py
+++ scripts/plot.py
@@ -60,6 +60,38 @@ def plot_query(log):
     # save
     fig.savefig("query.pdf", bbox_inches='tight')
 
+# plot the llc misses
+def plot_misses(log):
+    ssfe_query = extract_benchmark(log, "/ssfe_query/")
+    ssfe_query_batch = extract_benchmark(log, "/ssfe_query_batch/")
+    ssfe_dyn_query = extract_benchmark(log, "/ssfe_dong_query/")
+    ssfe_dyn_query_batch = extract_benchmark(log, "/ssfe_dong_query_batch/")
+    sepset_query = extract_benchmark(log, "/sepset_query/")
+    sepset_query_batch = extract_benchmark(log, "/sepset_query_batch/")
+    othello_query = extract_benchmark(log, "/othello_query/")
+
+    # in case it runs on the CI which doesn't have the access to the hardware counters.
+    if "LLC-misses" not in ssfe_query.columns.values:
+        return
+
+    # plot
+    matplotlib.rcParams.update({'font.size': 20}) 
+    fig, ax = plt.subplots(1, 1)
+    ax.grid(True, axis='both')
+    ax.set_xlabel("# Keys")
+    ax.set_ylabel("LLC-misses")
+    ax.plot(ssfe_query['keys'], ssfe_query['LLC-misses'], label='SSFE', marker='v', color='teal')
+    ax.plot(ssfe_query_batch['keys'], ssfe_query_batch['LLC-misses'], label='SSFE+batch', marker='^', color='rosybrown')
+    ax.plot(ssfe_dyn_query['keys'], ssfe_dyn_query['LLC-misses'], label='SSFE_DYN', marker='<', color='bisque')
+    ax.plot(ssfe_dyn_query_batch['keys'], ssfe_dyn_query_batch['LLC-misses'], label='SSFE_DYN+batch', marker='>', color='greenyellow')
+    ax.plot(sepset_query['keys'], sepset_query['LLC-misses'], label='SetSep', marker='1', color='orangered')
+    ax.plot(sepset_query_batch['keys'], sepset_query_batch['LLC-misses'], label='SetSep+batch', marker='2', color='fuchsia')
+    ax.plot(othello_query['keys'], othello_query['LLC-misses'], label='Othello', marker='3', color='navy')
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.4), ncol=2, shadow=True, fontsize=15)
+
+    # save
+    fig.savefig("misses.pdf", bbox_inches='tight')
+
 # plot gaussian elimination
 def plot_gaussian_elimination(df):
     def extract_performance(df, name):
@@ -91,4 +123,5 @@ if __name__ == "__main__":
 
     log = parse_bench_csv(args.csv)
     plot_query(log)
+    plot_misses(log)
     plot_gaussian_elimination(log)
